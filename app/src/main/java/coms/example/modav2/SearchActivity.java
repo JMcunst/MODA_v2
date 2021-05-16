ab33f82282ef35e2b4ead7d365c71e43a3a86bc7
JMcunst <dhk5844@ajou.ac.kr>
2021-05-16 18:41:10 +0900

    feat : search activity & xml

---
+++ app/src/main/java/coms/example/modav2/SearchActivity.java
@@ -0,0 +1,13 @@
+package coms.example.modav2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
