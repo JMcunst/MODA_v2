216fc0c3b3e2203d7c58c528f84eedf2a145b1a5
JMcunst <dhk5844@ajou.ac.kr>
2021-05-17 20:50:06 +0900

    feat : mainpage click funcs with (search, schedule, complete)

--- app/src/main/java/coms/example/modav2/MainActivity.java
+++ app/src/main/java/coms/example/modav2/MainActivity.java
@@ -1,17 +1,77 @@
 package coms.example.modav2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context = this;
 
+    public ImageView iv_search;
+    public ImageView iv_schedule;
+    public ImageView iv_complete;
+
+    RecyclerView re_schedule;
+    RecyclerView re_complete;
+
+    public int state_schedule_oc = 1; // OPEN
+    public int state_complete_oc = 1; // OPEN
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
 
+        setView();
+    }
+
+    public void setView(){
+        iv_search = (ImageView)findViewById(R.id.main_iv_search);
+        iv_schedule = (ImageView)findViewById(R.id.main_schedule_open_close);
+        iv_complete = (ImageView)findViewById(R.id.main_complete_open_close);
+        re_schedule = (RecyclerView)findViewById(R.id.main_schedule_recycler);
+        re_complete = (RecyclerView)findViewById(R.id.main_complete_recycler);
+
+        iv_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        iv_schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(state_schedule_oc == 1) {
+                    state_schedule_oc = 0;
+                    iv_schedule.setImageResource(R.drawable.ic_arrow_down_64);
+                    re_schedule.setVisibility(View.GONE);
+                }else{
+                    state_schedule_oc = 1;
+                    iv_schedule.setImageResource(R.drawable.ic_arrow_up_64);
+                    re_schedule.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        iv_complete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(state_complete_oc == 1) {
+                    state_complete_oc = 0;
+                    iv_complete.setImageResource(R.drawable.ic_arrow_down_64);
+                    re_complete.setVisibility(View.GONE);
+                }else{
+                    state_complete_oc = 1;
+                    iv_complete.setImageResource(R.drawable.ic_arrow_up_64);
+                    re_complete.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
     }
 }
--- app/src/main/java/coms/example/modav2/SearchActivity.java
+++ app/src/main/java/coms/example/modav2/SearchActivity.java
@@ -1,13 +1,48 @@
 package coms.example.modav2;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SearchActivity extends AppCompatActivity {
+    private Context context = this;
+
+    ImageView iv_back;
+    ImageView iv_search;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        setView();
+    }
+
+    public void setView(){
+        iv_back = (ImageView)findViewById(R.id.search_iv_back);
+        iv_search = (ImageView)findViewById(R.id.search_iv_search);
+
+        iv_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iv_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO_: edittext의 값을 읽어서 DB검색
+                Toast.makeText(getApplicationContext(),"검색기능입니다", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
 }
