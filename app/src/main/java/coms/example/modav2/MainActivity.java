2ddb307b235c1b3c50963357723ae5efa41367fd
JMcunst <dhk5844@ajou.ac.kr>
2021-05-20 21:08:46 +0900

    feat : main schedule category calendar add

--- app/src/main/java/coms/example/modav2/MainActivity.java
+++ app/src/main/java/coms/example/modav2/MainActivity.java
@@ -22,15 +22,20 @@ public class MainActivity extends AppCompatActivity {
     public ImageView iv_search;
     public ImageView iv_schedule;
     public ImageView iv_complete;
+    public ImageView iv_category_add;
+    public ImageView iv_calendar_add;
 
     public RecyclerView re_schedule;
     public RecyclerView re_complete;
     public MainScheduleAdapter resAdapter;
+    public MainScheduleAdapter recAdapter;
 
     public int state_schedule_oc = 1; // OPEN
     public int state_complete_oc = 1; // OPEN
+    public int state_cate_cal_direction = 0; // category
 
-    public ArrayList<MainScheduleDTO> mainScheduleDTOs = new ArrayList<>();
+    public ArrayList<MainScheduleDTO> mainSchs = new ArrayList<>();
+    public ArrayList<MainScheduleDTO> mainCompleteSchs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -46,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
         iv_complete = (ImageView)findViewById(R.id.main_complete_open_close);
         re_schedule = (RecyclerView)findViewById(R.id.main_schedule_recycler);
         re_complete = (RecyclerView)findViewById(R.id.main_complete_recycler);
+        iv_category_add = (ImageView)findViewById(R.id.main_category_add);
+        iv_calendar_add = (ImageView)findViewById(R.id.main_calendar_add);
 
 
         MainScheduleDTO ms1 = new MainScheduleDTO(1,"코품미팅","프로토타입",
@@ -55,13 +62,14 @@ public class MainActivity extends AppCompatActivity {
         MainScheduleDTO mc1 = new MainScheduleDTO(3,"코품회의커밋정하기","커밋",
                 new Date(121, 4,17),1,3,3,0);
 
-        mainScheduleDTOs.add(ms1);
-        mainScheduleDTOs.add(ms2);
-        mainScheduleDTOs.add(mc1);
+        mainSchs.add(ms1);
+        mainSchs.add(ms2);
+        mainCompleteSchs.add(mc1);
 
-        resAdapter = new MainScheduleAdapter(context, mainScheduleDTOs);
+        resAdapter = new MainScheduleAdapter(context, mainSchs);
+        recAdapter = new MainScheduleAdapter(context, mainCompleteSchs);
         re_schedule.setAdapter(resAdapter);
-        re_complete.setAdapter(resAdapter);
+        re_complete.setAdapter(recAdapter);
         re_schedule.setLayoutManager(new LinearLayoutManager(this));
         re_complete.setLayoutManager(new LinearLayoutManager(this));
 //        re_schedule.setLayoutManager(new LinearLayoutManager(this){
@@ -109,6 +117,26 @@ public class MainActivity extends AppCompatActivity {
                     state_complete_oc = 1;
                     iv_complete.setImageResource(R.drawable.ic_arrow_up_64);
                     re_complete.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        iv_category_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(state_cate_cal_direction == 1) {
+                    iv_category_add.setImageResource(R.drawable.ic_category_mintgray_checked_96);
+                    iv_calendar_add.setImageResource(R.drawable.ic_calendar_black_48);
+                    state_cate_cal_direction = 0;
+                }
+            }
+        });
+        iv_calendar_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(state_cate_cal_direction == 0) {
+                    iv_category_add.setImageResource(R.drawable.ic_category_black_64);
+                    iv_calendar_add.setImageResource(R.drawable.ic_calendar_mintgray_checked_96);
+                    state_cate_cal_direction = 1;
                 }
             }
         });
